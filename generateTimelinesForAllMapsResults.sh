5f8c659decce8fe31502157e44d5cc67517221d0
David M. Weigl <weigl@mdw.ac.at>
2019-05-22 16:03:06 +0200

    generate script - uncomment MAPS call

--- generateTimelinesForAllMapsResults.sh
+++ generateTimelinesForAllMapsResults.sh
@@ -3,10 +3,10 @@ for file in data/Results/*.json; do
   # for each participant
   pID=$(echo $file | sed -E 's/.*(P[0-9]+).boe.*/\1/')
   # generate json and ttl files
-#  python utils/convert_to_rdf.py -m $file -s http://localhost:8080/structure/WoO80-notes -t http://localhost:8080/timeline/BeethovenWettbewerb/$pID-notes -u http://localhost:8080/Beethoven_WoO80-32-Variationen-c-Moll.mei -f both -o $pID-notes
+  python utils/convert_to_rdf.py -m $file -s http://localhost:8080/structure/WoO80-notes -t http://localhost:8080/timeline/BeethovenWettbewerb/$pID-notes -u http://localhost:8080/Beethoven_WoO80-32-Variationen-c-Moll.mei -f both -o $pID-notes
   # append the .json or .ttl suffix as appropriate
-#  sed -i -e "s/$pID-notes/$pID-notes.ttl/g" $pID-notes.ttl
-#  sed -i -e "s/$pID-notes/$pID-notes.json/g" $pID-notes.json
+  sed -i -e "s/$pID-notes/$pID-notes.ttl/g" $pID-notes.ttl
+  sed -i -e "s/$pID-notes/$pID-notes.json/g" $pID-notes.json
   # find the line for this participant in the offsets file:
   echo "PID: $pID"
   offsetLine=$(grep $pID MIDI-Offsets-WoO80.tsv)
