e4c5d9c3b8b570af947c3a2578d682c2d4f7bbf6
David M. Weigl <weigl@mdw.ac.at>
2021-02-10 19:08:39 +0100

    properly post MIDI JSON object

--- src/containers/wrapper.js
+++ src/containers/wrapper.js
@@ -59,12 +59,12 @@ export default function Wrapper(props) {
     }
     timer = setTimeout(() => {
       if(midiEvents.length) {
-        let midiEventsJson = midiEvents.map(m => JSON.stringify(MIDIMessage(m)));
+        let midiEventsJson = midiEvents.map(m => { return {data: MIDIMessage(m), timestamp: m.timeStamp} });
         console.log("I declare a rehearsal to be complete: ", midiEventsJson);
         fetch(MIDI_BATCH_ENDPOINT, { 
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
-          body: midiEventsJson
+          body: JSON.stringify(midiEventsJson)
         }).then(response => response.json())
         .then(data => console.log("GOT RESPONSE: ", data))
       }
@@ -75,6 +75,7 @@ export default function Wrapper(props) {
   }, [midiEvents]);
     
     function handleMidiMessage(mes) { 
+      console.log("RAW MIDI MESSAGE: ", mes);
       setMidiEvents(midiEvents => [...midiEvents, mes]);
     }
 
@@ -118,82 +119,3 @@ export default function Wrapper(props) {
       </div>
     )
 }
-/*
-function connect() {
-  if("requestMIDIAccess" in navigator) { 
-    navigator.requestMIDIAccess()
-    .then(
-      (midi) => console.log("got midi: ", midi), // midiReady(midi),
-      (err) => console.log('Something went wrong', err));
-  } else { 
-    console.warn("Browser does not support WebMIDI");
-  }
-}
-
-function midiReady(midi) {
-  // Also react to device changes.
-  midi.addEventListener('statechange', (event) => initDevices(event.target));
-  initDevices(midi); // see the next section!
-}
-
-function initDevices(midi) {
-  // Reset.
-  let [midiIn, setMidiIn]  = useState([]);
-//  let [midiOut, setMidiOut]  = useState([]);
-
-  // MIDI devices that send you data.
-  const inputs = midi.inputs.values();
-  for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
-    setMidiIn([...midiIn, input.value]); 
-  }
-  // MIDI devices that you send data to.
-  const outputs = midi.outputs.values();
-  for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
-    midiOut.push(output.value);
-  }
-  displayDevices();
-//  startListening();
-}
-
-function displayDevices() { 
-  let [midiIn, setMidiIn]  = useState([]);
-  console.log("Display devices! Midi in: ", midiIn);
-}
-
-// Start listening to MIDI messages.
-function startListening() {
-  for (const input of midiIn) {
-    input.addEventListener('midimessage', midiMessageReceived);
-  }
-}
-
-function midiMessageReceived(event) {
-  // MIDI commands we care about. See
-  // http://webaudio.github.io/web-midi-api/#a-simple-monophonic-sine-wave-midi-synthesizer.
-  const NOTE_ON = 9;
-  const NOTE_OFF = 8;
-
-  const cmd = event.data[0] >> 4;
-  const pitch = event.data[1];
-  const velocity = (event.data.length > 2) ? event.data[2] : 1;
-
-  // You can use the timestamp to figure out the duration of each note.
-  const timestamp = Date.now();
-
-  // Note that not all MIDI controllers send a separate NOTE_OFF command for every NOTE_ON.
-  if (cmd === NOTE_OFF || (cmd === NOTE_ON && velocity === 0)) {
-    console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: ${velocity}`);
-
-    // Complete the note!
-    const note = notesOn.get(pitch);
-    if (note) {
-      console.log(`🎵 pitch:${pitch}, duration:${timestamp - note} ms.`);
-      notesOn.delete(pitch);
-    }
-  } else if (cmd === NOTE_ON) {
-    console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: {velocity}`);
-
-    // One note can only be on at once.
-    notesOn.set(pitch, timestamp);
-  }
-}*/
