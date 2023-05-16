f8c65e754d1f8b3de1fff6edd45d20d51cd5de39
Alastair Porter <alastair@porter.net.nz>
2023-05-16 19:30:43 +1200

    Upgrade react

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'stop-runaway-react-effects/hijack'
 import React  from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -10,10 +10,11 @@ import { reducers } from 'meld-clients-core/lib/reducers';
 import App from "./containers/App";
 
 const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
+const container = document.querySelector('.container');
+const root = createRoot(container!);
 
-ReactDOM.render(
+root.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
           <App />
 	</Provider>
-		, document.querySelector('.container')
 );
