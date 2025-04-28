ba4f15148ffe572024200f866d605bf8c500e6f3
Alastair Porter <alastair@porter.net.nz>
2025-04-28 11:55:42 +0200

    Hide useless warning messages

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,28 @@
 module.exports = function override(config, env) {
     config.resolve.fallback = {"crypto": require.resolve("crypto-browserify")}
+
+    config.ignoreWarnings = [
+        /*
+        Ignores errors of the format
+        WARNING in ./node_modules/@magenta/music/esm/spice/model.js
+        Module Warning (from ./node_modules/source-map-loader/dist/cjs.js):
+        Failed to parse source map from '[...]node_modules/@magenta/music/src/spice/model.ts' file: Error:
+           ENOENT: no such file or directory, open '[...]node_modules/@magenta/music/src/spice/model.ts'
+        for any @magenta/music or @inrupt/solid-ui-react modules.
+        */
+        function ignoreSourceMapWarning(warning) {
+            if (warning.message && warning.message.includes('Failed to parse source map')) {
+                if (warning.message.includes('@inrupt/')) {
+                    return true;
+                }
+                if (warning.message.includes('@magenta/')) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+    ];
+
     return config;
 }
