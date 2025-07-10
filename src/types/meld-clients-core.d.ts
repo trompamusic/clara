0a58e389b5f8035da79583ed10bb0e3d9ea55663
Alastair Porter <alastair@porter.net.nz>
2025-07-10 11:02:38 +0200

    Add type stubs for meld-clients-core/lib/actions

---
+++ src/types/meld-clients-core.d.ts
@@ -0,0 +1,97 @@
+declare module 'meld-clients-core/lib/actions';
+
+declare module 'meld-clients-core/lib/actions/index' {
+  export function scoreNextPageStatic(uri: string, currentPage: number, mei: any): {
+    type: string;
+    payload: {
+      uri: string;
+      pageNum: number;
+      mei: any;
+    };
+  };
+
+  export function scorePrevPageStatic(uri: string, currentPage: number, mei: any): {
+    type: string;
+    payload: {
+      uri: string;
+      pageNum: number;
+      mei: any;
+    };
+  };
+
+  export function postAnnotation(
+    submitUri: string,
+    motivation: string,
+    body: any,
+    target: string,
+    callback: (response: any) => void
+  ): {
+    type: string;
+    payload: {
+      submitUri: string;
+      motivation: string;
+      body: any;
+      target: string;
+      callback: (response: any) => void;
+    };
+  };
+
+  export function scoreSetOptions(
+    uri: string,
+    options: {
+      scale?: number;
+      adjustPageHeight?: number;
+      pageHeight?: number;
+      pageWidth?: number;
+      footer?: string;
+      unit?: number;
+      [key: string]: any;
+    }
+  ): {
+    type: string;
+    payload: {
+      uri: string;
+      options: any;
+    };
+  };
+}
+
+declare module 'meld-clients-core/lib/containers/score' {
+  import { Component } from 'react';
+
+  interface ScoreProps {
+    uri: string;
+    key?: string;
+    options?: {
+      scale?: number;
+      adjustPageHeight?: number;
+      pageHeight?: number;
+      pageWidth?: number;
+      footer?: string;
+      unit?: number;
+      [key: string]: any;
+    };
+  }
+
+  class Score extends Component<ScoreProps> {}
+
+  export default Score;
+}
+
+declare module 'dragselect' {
+  export interface DragSelectOptions {
+    selectables?: Element[] | NodeListOf<Element>;
+    area?: HTMLElement;
+    selectedClass?: string;
+    onDragStartBegin?: () => void;
+    callback?: (elements: Element[]) => void;
+    [key: string]: any;
+  }
+
+  class DragSelect {
+    constructor(options: DragSelectOptions);
+    stop(): void;
+  }
+
+  export default DragSelect;
+}
